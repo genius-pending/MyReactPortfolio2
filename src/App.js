51200e74bd71b11446423aa38beb7cbbb59cddf4
Shivey Singh <shivey_khangura@hotmail.com>
2021-03-01 18:14:29 +0000

    hash router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
-import React from 'react';
 import NavBar from './components/NavBar';
 import HomePage from './components/HomePage';
 import ProjectPage from './components/ProjectPage';
 import ContactPage from './components/ContactPage';
 import './App.css';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, } from "react-router-dom";
+import React, { Component } from 'react';
+import { HashRouter, Route, Link } from "react-router-dom";
+
+class App extends Component {
+  render() {
+    return (
+      <HashRouter basename="/">
+        <div>
+        <NavBar />
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/ProjectPage">ProjectPage</Link></li>
+            <li><Link to="/ContactPage">ContactPage</Link></li>
+            
+          </ul>
+
+          <hr />
+          <Route exact path= {["/", "/about", "/react_portfolio"]} component={HomePage} />
+          <Route exact path="/ProjectPage"  component={ProjectPage} />
+          <Route exact path="/ContactPage"  component={ContactPage} />
+        </div>
+      </HashRouter>
+    );
+  }
+}
+
+export default App;
 
 
-function App() {
+/*function App() {
   return (
     <Router>
       <div>
@@ -22,4 +49,4 @@ function App() {
   );
 }
 
-export default App;
+export default App;*/
