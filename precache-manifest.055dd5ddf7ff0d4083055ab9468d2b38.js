6fc48444db875f03c0681eb65110ed8de196bb2b
Shivey Singh <shivey_khangura@hotmail.com>
2021-03-01 17:47:35 +0000

    Updates

---
+++ precache-manifest.055dd5ddf7ff0d4083055ab9468d2b38.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1cc73b02ab391b9119779f7d43e8b00",
+    "url": "/MyReactPortfolio2/index.html"
+  },
+  {
+    "revision": "0a68110adea1ef96e85b",
+    "url": "/MyReactPortfolio2/static/css/main.f4da40e1.chunk.css"
+  },
+  {
+    "revision": "ac09fb113e555a731c3b",
+    "url": "/MyReactPortfolio2/static/js/2.4a2f6a79.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/MyReactPortfolio2/static/js/2.4a2f6a79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a68110adea1ef96e85b",
+    "url": "/MyReactPortfolio2/static/js/main.f0757d32.chunk.js"
+  },
+  {
+    "revision": "102d4fa9a0144fecbdc8",
+    "url": "/MyReactPortfolio2/static/js/runtime-main.cdffcff5.js"
+  },
+  {
+    "revision": "efdba04681a3215e786c0e996e4bc780",
+    "url": "/MyReactPortfolio2/static/media/background2.efdba046.jpg"
+  },
+  {
+    "revision": "395df93f3a2d8a841031e95505e05fa4",
+    "url": "/MyReactPortfolio2/static/media/background3.395df93f.jpg"
+  },
+  {
+    "revision": "4014d4a006e33d57f3734e27eeece68c",
+    "url": "/MyReactPortfolio2/static/media/birminghamMap.4014d4a0.jpg"
+  },
+  {
+    "revision": "61f22be75db594f19779cd8ff137e271",
+    "url": "/MyReactPortfolio2/static/media/myLogoName.61f22be7.jpg"
+  },
+  {
+    "revision": "779fdd0b12c193cbc42bc4ca3cbac363",
+    "url": "/MyReactPortfolio2/static/media/myProfile.779fdd0b.jpg"
+  },
+  {
+    "revision": "5b9df73678c495eef587d684b1b00773",
+    "url": "/MyReactPortfolio2/static/media/screenshot 1.5b9df736.jpg"
+  },
+  {
+    "revision": "8e42441dfdef479bccd7ceba960c7c10",
+    "url": "/MyReactPortfolio2/static/media/screenshot 2.8e42441d.jpg"
+  },
+  {
+    "revision": "8a3c2b05693c874a8d345898354cef89",
+    "url": "/MyReactPortfolio2/static/media/screenshot 3.8a3c2b05.jpg"
+  },
+  {
+    "revision": "72e10245f02bba157b272da0bd3c04f6",
+    "url": "/MyReactPortfolio2/static/media/screenshot 4.72e10245.jpg"
+  },
+  {
+    "revision": "39dd60e1f4c7d1e00efffed810d98295",
+    "url": "/MyReactPortfolio2/static/media/screenshot 5.39dd60e1.jpg"
+  },
+  {
+    "revision": "5c11282667571ce115f7bdad885062fe",
+    "url": "/MyReactPortfolio2/static/media/screenshot.5c112826.jpg"
+  }
+]);
